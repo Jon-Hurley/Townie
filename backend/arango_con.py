0c55952f16a09cdb7a8c5d66f054f8d53362df9c
Jack Rookstool <jrooksto@purdue.edu>
2023-02-15 22:32:27 -0500

    hi!

--- backend/arango_con.py
+++ backend/arango_con.py
@@ -20,12 +20,22 @@ users = db.collection('User')
 #   'purchases': []
 # })
 
-query = "FOR user IN User FILTER CONTAINS(LOWER(user.username), LOWER(@substr)) RETURN { user }"
+getUsersBySubstring = "FOR user IN User FILTER CONTAINS(LOWER(user.username), LOWER(@substr)) RETURN { username: user.username, id: user._key }"
+
+getUserWithID = "FOR user IN User FILTER user._key == '10907' RETURN { user }"
 
 cursor = db.aql.execute(
-    query, bind_vars={'substr': 'arnav'}
+    getUsersBySubstring, bind_vars={'substr': 'arnav'}
 )
 
+cursor2 = db.aql.execute(
+    getUserWithID
+)
+
+print("Printing substrings test!")
 print(cursor.batch())
+
+print("Printing user id test!")
+print(cursor2.batch())
 
 # print(res)
