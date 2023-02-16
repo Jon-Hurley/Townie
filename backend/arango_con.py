5c9a004e45300ca2adc900819b8e5566da38c603
ArnavMeh <mehraarn000@gmail.com>
2023-02-15 21:57:27 -0500

    help me, im swimming in shark piss

--- backend/arango_con.py
+++ backend/arango_con.py
@@ -4,11 +4,20 @@ import os
 
 load_dotenv()
 
-# Initialize the ArangoDB client.
-client = ArangoClient(hosts=os.environ.get('ARANGO_ENDPOINT'))
+host = os.environ.get('ARANGO_ENDPOINT')
+password = os.environ.get('ARANGO_PASSWD')
 
-# Connect to "_system" database as root user.
-# This returns an API wrapper for "_system" database.
-sys_db = client.db('_system', username='root', password=os.environ.get('ARANGO_PASSWD'))
+client = ArangoClient(hosts=[host])
+db = client.db('_system', username='root', password=password)
 
-print(sys_db)
+users = db.collection('User')
+res = users.insert({
+  'username': 'Arnav',
+  'passwordHash': 'password',
+  'phone': '+13176909263',
+  'points': 50,
+  'rank': 'explorer',
+  'purchases': []
+})
+
+print(res)
