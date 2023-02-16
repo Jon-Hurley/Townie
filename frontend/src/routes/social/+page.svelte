36f90f33212932257834677e451bfe87b89e16a5
ArnavMeh <mehraarn000@gmail.com>
2023-02-16 16:05:00 -0500

    css my ass

--- frontend/src/routes/social/+page.svelte
+++ frontend/src/routes/social/+page.svelte
@@ -27,7 +27,7 @@
 
 
 <div
-    class="container"
+    class="m-0 w-full p-2"
 >
     <input
         bind:value={userSearch}
@@ -36,12 +36,17 @@
             timeout = setTimeout(getUsers, 500);
         }}
         type="text"
-        class="w-full p-2 pl-8 rounded border border-gray-200 bg-gray-200 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent"
+        class="m-0 w-full p-2 pl-8 rounded border border-gray-200 bg-gray-200 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent"
         placeholder="Search Users"
     >
-    {#if userSearch.length}
+</div>
+
+{#if userSearch.length}
+    <div
+        class="absolute top-12 m-0 w-full p-2"
+    >
         <ul
-            class="absolute top-12 p-8 w-full z-10 mt-1 rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            class="w-full rounded-md bg-white text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
             tabindex="-1"
             role="listbox"
             aria-labelledby="listbox-label"
@@ -49,14 +54,14 @@
         >
             {#if results.length === 0}
                 <li
-                    class="w-full text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9"
+                    class="w-full text-gray-900 relative cursor-default select-none p-2"
                 >
                     No Results Found.
                 </li>
             {:else}
                 {#each results as r}
                     <li
-                        class="w-full text-gray-900 relative cursor-pointer py-2 pl-3 pr-9"
+                        class="w-full text-gray-900 relative cursor-pointer p-2"
                         on:click={() => {
                             console.log("going")
                             goto('/user/' + r.id)
@@ -72,53 +77,5 @@
                 {/each}
             {/if}
         </ul>
-    {/if}
-</div>
-
-
-<!-- <div class="">
-    <div class="inline-flex flex-col justify-center relative text-gray-500">
-        <div class="relative">
-            <input type="text" class="p-2 pl-8 rounded border border-gray-200 bg-gray-200 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent" placeholder="search..." value="Gar" />
-            <svg class="w-4 h-4 absolute left-2.5 top-3.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
-            </svg>
-        </div>
-        <ul class="bg-white border border-gray-100 w-full mt-2">
-            <li class="pl-8 pr-2 py-1 border-b-2 border-gray-100 relative cursor-pointer hover:bg-yellow-50 hover:text-gray-900">
-                <svg class="absolute w-4 h-4 left-2 top-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"/>
-                </svg>
-                <b>Gar</b>dameer - Italië
-            </li>
-            <li class="pl-8 pr-2 py-1 border-b-2 border-gray-100 relative cursor-pointer hover:bg-yellow-50 hover:text-gray-900">
-                <svg class="absolute w-4 h-4 left-2 top-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"/>
-                </svg>
-                <b>Gar</b>da - Veneto - Italië
-            </li>
-            <li class="pl-8 pr-2 py-1 border-b-2 border-gray-100 relative cursor-pointer hover:bg-yellow-50 hover:text-gray-900">
-                <svg class="absolute w-4 h-4 left-2 top-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"/>
-                </svg>
-                <b>Gar</b>da Hotel - Italië
-            </li>
-            <li class="pl-8 pr-2 py-1 border-gray-100 relative cursor-pointer hover:bg-yellow-50 hover:text-gray-900">
-                <svg class="absolute w-4 h-4 left-2 top-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"/>
-                </svg>
-                <b>Gar</b>dena Resort - Italië
-            </li>
-        </ul>
     </div>
-</div> -->
-
-
-<style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding: 5px;
-    }
-</style>
+{/if}
