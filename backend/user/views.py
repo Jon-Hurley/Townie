a3f920179b722714c386b899fd1af2371c8d6ed7
Sai Girap <saigirap@gmail.com>
2023-02-18 18:47:06 -0500

    Login and signup methods almost finished

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,17 +1,36 @@
+import json
+import arango_con
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .twilio_con import t
 
 def signup(request):
-    return JsonResponse({
-        ":)": ":)"
-    })
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+        phoneNumber = data.get('phoneNumber')
+
+        res = arango_con.getUsersBySubstring(username).batch()
+        if len(list(res)) is 0:
+            return JsonResponse({'success': False})
+        arango_con.createUser(username, password, phoneNumber)
+        user = arango_con.login(request, username=username, password=password)
+        login(request, user)
+    return JsonResponse({'success': True})
 
 @csrf_exempt # note csrf is being wonky, add this to POST/PUT/DELETE reqs for now
 def login(request):
-    return JsonResponse({
-        ":)": ":)"
-    })
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+        user = arango_con.login(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'success': True})
+    return JsonResponse({'success': False})
+    
 
 def loginWithToken(request):
     return JsonResponse({
