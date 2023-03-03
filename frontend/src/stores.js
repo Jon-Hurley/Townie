7737584bffe9dad2bb106a3090b88f477021d0f7
Jack Rookstool <jrooksto@purdue.edu>
2023-03-03 12:37:44 -0500

    userStore boi

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -2,7 +2,49 @@ import { get, writable } from 'svelte/store';
 
 export const userStore = writable();
 
-export const gameStore = writable();
+export const gameStore = writable({
+    game: {
+    "maxTime": 0,
+    "numFinished": 0,
+    "page": "lobby",
+    "settings": {
+      "radius": 5,
+      "walkingAllowed": true,
+      "drivingAllowed": false,
+      "bicyclingAllowed": false,
+      "transitAllowed": false,
+      "theme": "None",
+      "desiredCompletionTime": 180,
+      "lon": 0,
+      "lat": 0
+    },
+    "startTime": 0,
+    "trueCompletionTime": 0
+  },
+destinations: [{
+    "latitude": 38.2555867,
+    "longitude": -85.7585581,
+    "name": "Vincenzo's",
+    "theme": "restaurant"
+  }, {
+    "latitude": 38.2581251,
+    "longitude": -85.7571317,
+    "name": "Swizzle Dinner & Drinks",
+    "theme": "restaurant"
+  }, {
+    "latitude": 38.2560922,
+    "longitude": -85.75278279999999,
+    "name": "Merle's Whiskey Kitchen",
+    "theme": "restaurant"
+  }],
+players: [{
+    "connectionId": "BKlHAczWIAMCKiQ=",
+    "destinationIndex": 0,
+    "points": 0,
+    "lon": 0,
+    "lat": 0
+  }]
+});
 
 export const locationStore = writable();
 
