8a936527c518c81a6bf527ebfe2c97278455e901
Sai Girap <saigirap@gmail.com>
2023-03-21 10:37:58 -0400

    couple Changes

--- frontend/src/routes/summary/[slug]/summary.svelte
+++ frontend/src/routes/summary/[slug]/summary.svelte
@@ -13,6 +13,30 @@
 		message: null,
 		dest: null
 	};
+	function amperoctoplus(s) {
+		s = s.replace(/&/g, '%26');
+		s = s.replace(/#/g, '%23');
+		s = s.replace(/\+/g, '%2B');
+		s = s.replace(/@/g, '%40');
+		s = s.replace(/:/g, '%3A');
+		return s;
+	}
+
+	/*
+        Twitter: 
+        https://twitter.com/intent/tweet?text=Check%20out%20this%20Townie%20game%20I%20played%3A%0A + link
+    
+        Whatsapp: 
+        https://api.whatsapp.com/send/?text=Check%20out%20this%20Townie%20game%20I%20played%3A%0A + link + &type=custom_url
+    
+        Mail:
+        mailto:?&body=Check%20out%20this%20Townie%20game%20I%20played%3A%0A + link
+
+        Reddit:
+        https://www.reddit.com/submit?&title=Check%20out%20this%20Townie%20game%20I%20played%3A%0A&url= + link
+
+        Copy Link:
+    */
 </script>
 
 <Modal {...messageObj} />
@@ -23,6 +47,93 @@
 	</div>
 	<div class="text-gray-700 text-md text-center">
 		#{summary.key}
+	</div>
+</div>
+
+<div class={title}>Share Game!</div>
+<hr class={hr} />
+<div class="h-full overflow-auto">
+	<div class="flex flex-wrap justify-center gap-2 px-2 py-4">
+		<div
+			class="
+                border-gray-200 border-4 rounded-full
+                p-3 m-0
+                text-indigo-500 font-semibold
+                w-24 h-24
+                flex flex-col items-center justify-center
+                text-xs
+                hover:scale-110 duration-200
+                z-100
+                overflow-visible
+                cursor-pointer
+            "
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d={p.rating} />
+			</svg>
+
+			Twitter
+		</div>
+
+		<div
+			class="
+                border-gray-200 border-4 rounded-full
+                p-3 m-0
+                text-indigo-500 font-semibold
+                w-24 h-24
+                flex flex-col items-center justify-center
+                text-xs
+                hover:scale-110 duration-200
+                z-100
+                overflow-visible
+                cursor-pointer
+            "
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d={p.rating} />
+			</svg>
+
+			Facebook
+		</div>
+
+		<div
+			class="
+                border-gray-200 border-4 rounded-full
+                p-3 m-0
+                text-indigo-500 font-semibold
+                w-24 h-24
+                flex flex-col items-center justify-center
+                text-xs
+                hover:scale-110 duration-200
+                z-100
+                overflow-visible
+                cursor-pointer
+            "
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d={p.rating} />
+			</svg>
+
+			LinkedIn
+		</div>
 	</div>
 </div>
 
