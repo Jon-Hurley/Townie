0ea52064b3a124ca66367927be918a29332fa652
ArnavMeh <mehraarn000@gmail.com>
2023-03-29 18:53:44 -0400

    xsxsxs

--- frontend/src/routes/game/+page.svelte
+++ frontend/src/routes/game/+page.svelte
@@ -1,9 +1,2 @@
 <script>
-	import { onMount } from 'svelte';
-    import { goto } from '$app/navigation';
-	import { Game } from '../../classes/Game';
-
-	onMount(() => {
-		goto('game/' + Game.getPage())
-    });
 </script>
