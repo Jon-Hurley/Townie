6a36f0c44a23a553ff15f28d35c644d7b2ebd985
Jon-Hurley <89647725+Jon-Hurley@users.noreply.github.com>
2023-04-05 19:30:56 -0400

    experiment

---
+++ backend/group/scraperfsq.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+def generate():
+
+    url = "https://api.foursquare.com/v3/places/search"
+
+    params = {
+        "query": "coffee",
+        "ll": "47.606,-122.349358",
+        "open_now": "true",
+        "sort": "DISTANCE",
+        "limit": 50
+    }
+
+    headers = {
+        "Accept": "application/json",
+        "Authorization": os.environ.get("TOWNIE_API_KEY")
+    }
+
+    response = requests.request("GET", url, params=params, headers=headers)
+    response1 = json.loads(response.text)
+    for i in range(len(response1['results'])):
+        print(response1['results'][i]['name'])
+
+def main():
+    generate()
+
+if __name__ == "__main__":
+    main()
