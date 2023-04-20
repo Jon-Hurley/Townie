e6bb2a5e4ef3cc803be28fa81def6995c38e1a5f
ArnavMeh <mehraarn000@gmail.com>
2023-04-20 13:24:21 -0400

    hi mom

--- frontend/src/general-components/modal.svelte
+++ frontend/src/general-components/modal.svelte
@@ -10,11 +10,11 @@
 		cancelText = 'Cancel',
 		includeCancel = status === 2;  // typically only confirmations have a cancel button
 
-	const colorStyle = [redStyle, greenStyle, grayStyle, amberStyle][status];
-	const color = ['red', 'green', 'gray', 'amber'][status];
-	const name = ['Error', 'Success', 'Confirm', 'Warning'][status];
-	const bStyle = `${buttonStyle} ${colorStyle}`;
-	const svg = [
+	$: colorStyle = [redStyle, greenStyle, grayStyle, amberStyle][status];
+	$: color = ['red', 'green', 'gray', 'amber'][status];
+	$: name = ['Error', 'Success', 'Confirm', 'Warning'][status];
+	$: bStyle = `${buttonStyle} ${colorStyle}`;
+	$: svg = [
 		'M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z',
 		'M4.5 12.75l6 6 9-13.5',
 		'M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z',
