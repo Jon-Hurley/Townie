862ae1cbcc99379284e4e8f20f3b366081ac9dd6
Sai Girap <saigirap@gmail.com>
2023-04-20 16:37:34 -0400

    munch and hound dog sounds

--- frontend/src/routes/app/audio.svelte
+++ frontend/src/routes/app/audio.svelte
@@ -1,37 +1,40 @@
 <script>
-    import { primaryAudio } from '../../stores';
-    import Kahoot from '$lib/lobby-classic-game.mp3';
-    import Moonshine from '$lib/moonshine.mp3';
-    import ThemeSong from '$lib/theme-song.mp3';
+	import { primaryAudio } from '../../stores';
+	import Kahoot from '$lib/lobby-classic-game.mp3';
+	import Moonshine from '$lib/moonshine.mp3';
+	import ThemeSong from '$lib/theme-song.mp3';
+	import HoundDog from '$lib/hound-dog.mp3';
+	import Munch from '$lib/munch.mp3';
 	import { onDestroy } from 'svelte';
 
-    let audioElement;
+	let audioElement;
 
-    $: if ($primaryAudio) {
-        if (audioElement) {
-            audioElement.pause();
-            audioElement.src = (
-                $primaryAudio === 'Kahoot' ? Kahoot
-                : $primaryAudio === 'Moonshine' ? Moonshine
-                : $primaryAudio === 'Townie Theme' ? ThemeSong
-                : ''
-            );
-            audioElement.play();
-        }
-    } else {
-        if (audioElement) {
-            audioElement.pause();
-        }
-    }
+	$: if ($primaryAudio) {
+		if (audioElement) {
+			audioElement.pause();
+			audioElement.src =
+				$primaryAudio === 'Kahoot'
+					? Kahoot
+					: $primaryAudio === 'Moonshine'
+					? Moonshine
+					: $primaryAudio === 'Townie Theme'
+					? ThemeSong
+					: $primaryAudio === 'Hound Dog'
+					? HoundDog
+					: $primaryAudio === 'Munch'
+					? Munch
+					: '';
+			audioElement.play();
+		}
+	} else {
+		if (audioElement) {
+			audioElement.pause();
+		}
+	}
 
-    onDestroy(() => {
-        audioElement?.pause();
-    });
+	onDestroy(() => {
+		audioElement?.pause();
+	});
 </script>
 
-<audio
-    bind:this={audioElement}
-    loop="true"
-    autostart="true"
-    style="height: 0px; width: 0px;"
-/>
+<audio bind:this={audioElement} loop="true" autostart="true" style="height: 0px; width: 0px;" />
