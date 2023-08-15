dcb7146c1c923a3a081d04cde7f04b50ed491f07
ArnavMeh <mehraarn000@gmail.com>
2023-08-14 21:28:09 -0400

    sveltekit netlify adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,12 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: true,
+	   
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
+	}
 };
 
 export default config;
